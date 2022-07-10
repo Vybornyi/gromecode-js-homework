8c7031bd0740d02ff64da2e60345c95c3c982a75
Vybornyi <1vybornyi@gmail.com>
2022-07-10 17:54:37 +0300

    add

--- 21/task-3/index.js
+++ 21/task-3/index.js
@@ -1,36 +1,14 @@
-const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
-];
+export function getItemsList() {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+}
+getItemsList();
 
-/**
- * @param {object[]} tasksList
- * @return {undefined}
- */
-const renderTasks = tasksList => {
-  // put your code here
-  const listElem = document.querySelector('.list');
-
-  const listItemsElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const chexboxElem = document.createElement('input');
-      chexboxElem.setAttribute('type', 'checkbox');
-
-      chexboxElem.checked = done;
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      chexboxElem.classList.add('.list__item-checkbox');
-      listItemElem.append(chexboxElem, text);
-
-      return listItemElem;
-    });
-  listElem.append(...listItemsElems);
-};
-renderTasks(tasks);
+export function getItemsArray() {
+  const NodeList = document.querySelectorAll('.tool');
+  const elementsArray = Array.from(NodeList);
+  console.dir(elementsArray);
+  return elementsArray;
+}
+getItemsArray();
