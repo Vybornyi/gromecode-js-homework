5a88298536f2cdcc7a0bb09c9014bf1841fa34a9
Vybornyi <1vybornyi@gmail.com>
2022-07-10 18:14:01 +0300

    add

--- 21/task-4/index.js
+++ 21/task-4/index.js
@@ -1,36 +1,19 @@
-const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
-];
+export function getTitle() {
+  return document.querySelector('.title').textContent;
+}
+console.log(getTitle());
 
-/**
- * @param {object[]} tasksList
- * @return {undefined}
- */
-const renderTasks = tasksList => {
-  // put your code here
-  const listElem = document.querySelector('.list');
+export function getDescription() {
+  return document.querySelector('.about').innerText;
+}
+console.log(getDescription());
 
-  const listItemsElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const chexboxElem = document.createElement('input');
-      chexboxElem.setAttribute('type', 'checkbox');
+export function getPlans() {
+  return document.querySelector('.plans').innerHTML;
+}
+console.log(getPlans());
 
-      chexboxElem.checked = done;
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      chexboxElem.classList.add('.list__item-checkbox');
-      listItemElem.append(chexboxElem, text);
-
-      return listItemElem;
-    });
-  listElem.append(...listItemsElems);
-};
-renderTasks(tasks);
+export function getGoal() {
+  return document.querySelector('.goal').outerHTML;
+}
+console.log(getGoal());
